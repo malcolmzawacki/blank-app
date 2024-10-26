0172f3e0d6b5f54b92a9926d021f07e019e4916e
malcolmzawacki <115760130+malcolmzawacki@users.noreply.github.com>
2024-10-26 18:17:52 +0000

    changed main file, added a page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
-
+st.set_page_config(page_title="Home")
+st.sidebar.header("Home")
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+
+
+st.write(f"Streamlit version: {st.__version__}")
+st.title("Welcome to the Physics Practice Tool")
+
+st.write("""
+Select a problem type from the sidebar to begin practicing.
+""")
+
